4e71961c96af6f51acd5fa047b7a0d87a6de2a66
Bryan <suprnewb@gmail.com>
2022-06-09 15:39:29 +1000

    Create Modal

--- script.js
+++ script.js
@@ -6,30 +6,55 @@ function book(title, author, pages, read) {  //Book constructor
     this.pages = pages
     this.read = read
     this.info = function() {
-        if(read == true) {
+        if(read === true) {
             return `${title} by ${author}, ${pages}, has read this book`;
         } else {
             return `${title} by ${author}, ${pages}, has not read this book yet`;
         }
     }
 }
-// function submit() {
-//     document.getElementById('bookForm').submit();
-// }
+// Modal
+const openEls = document.querySelectorAll('[data-open]');
+const isVisible = 'is-visible';
+
+for(const el of openEls) {
+    el.addEventListener('click', function() {
+        const modalId = this.dataset.open;
+        document.getElementById(modalId).classList.add(isVisible);
+    });
+}
+
+const closeEls = document.querySelectorAll('[data-close]');
+
+for (const el of closeEls) {
+    el.addEventListener('click', function() {
+        this.parentElement.classList.remove(isVisible);
+    });
+}
+
+document.addEventListener('click', e => {
+    if(e.target == document.querySelector(".modal.is-visible")) {
+        document.querySelector('modal.is-visible').classList.remove(isVisible);
+    }
+});
 
 function addBookToLibrary() {  //Input new book
-    let title = document.getElementsByClassName('title');  //Take input
+    myLibrary.push(new book(title, author, pages, read));  //Add to array
+}
+
+function submitForm() {
+    let title = document.getElementsByClassName('title').value;  //Take input
     let author = document.getElementsByClassName('author');
     let pages = document.getElementsByClassName('pages');
     let read = document.getElementsByClassName('read');
-
-
-    const newBook = new book(title, author, pages, read);  //Run through constructor
-    myLibrary.push(newBook);  //Add to array
+    addBookToLibrary(title, author, pages, read);
 }
 
 function resetForm() {
-    document.getElementsByClassName('reset').reset();
+    const resetButton = document.getElementsByClassName('reset')
+    resetButton.addEventListener('click', event => {
+        document.getElementById('bookForm').reset()
+    })
 }
 
 const theHobbit = new book('The Hobbit', 'J.R.R Tolkien', '295 pages', 'true')
